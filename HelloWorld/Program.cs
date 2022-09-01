a717dabd6778384475a4f1cfd1e623748089ce14
Voragun Supawong <voragun2543@gmail.com>
2022-09-01 16:02:59 +0700

    ❤️ [if-else statement] Dice game

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,13 +7,24 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Random dice = new Random();
-            int roll1 = dice.Next();
-            int roll2 = dice.Next(101);
-            int roll3 = dice.Next(50, 101);
 
-            Console.WriteLine($"First roll: {roll1}");
-            Console.WriteLine($"Second roll: {roll2}");
-            Console.WriteLine($"Third roll: {roll3}");
+            int roll1 = dice.Next(1, 7);
+            int roll2 = dice.Next(1, 7);
+            int roll3 = dice.Next(1, 7);
+
+            int total = roll1 + roll2 + roll3;
+
+            Console.WriteLine($"Dice roll : {roll1} + {roll2} + {roll3} = {total}");
+
+            if (total > 14)
+            {
+                Console.WriteLine("You Win!");
+            }
+
+            if (total < 15)
+            {
+                Console.WriteLine("Sorry, you lose.");
+            }
         }
     }
 }
