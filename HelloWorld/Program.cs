822d3bd468a54e1af97f8a936f6cd34751b8c49a
Voragun Supawong <voragun2543@gmail.com>
2022-09-01 16:51:51 +0700

    ⚽[array] use StartsWith() method

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,16 +6,16 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int[] inventory = { 200, 450, 700, 175, 250 };
-            int sum = 0;
-            int bin = 0;
-            foreach (int items in inventory)
+            string[] order = { "B123", "C234", "A345", "C15",
+                    "B177", "G3003", "C235", "B179" };
+
+            foreach (string items in order)
             {
-                sum += items;
-                bin++;
-                Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+                if (items.StartsWith("B"))
+                {
+                    Console.WriteLine(items);
+                }
             }
-            Console.WriteLine($"We have {sum} items in inventory.");
         }
     }
 }
