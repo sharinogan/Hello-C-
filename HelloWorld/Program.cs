03f6df4001015e409ad0800f1f000d6de884b839
Voragun Supawong <voragun2543@gmail.com>
2022-09-01 15:08:46 +0700

    [variable syntax] casting variable output

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -12,19 +12,20 @@ namespace HelloWorld
             string projectName = "First experience";
             Console.Write($"This is {projectName} Program\n"); // string interpolation
 
-            int first = 10;
-            int second = 20;
-            Console.WriteLine(first + second);
+            //int sum = 7 + 5;
+            //int difference = 7 - 5;
+            //int product = 7 * 5;
+            //int quotient = 7 / 5;
 
-            int sum = 7 + 5;
-            int difference = 7 - 5;
-            int product = 7 * 5;
-            int quotient = 7 / 5;
+            //Console.WriteLine("Sum : " + sum);
+            //Console.WriteLine("Difference : " + difference);
+            //Console.WriteLine("Product : " + product);
+            //Console.WriteLine("Quotient : " + quotient);
 
-            Console.WriteLine("Sum : " + sum);
-            Console.WriteLine("Difference : " + difference);
-            Console.WriteLine("Product : " + product);
-            Console.WriteLine("Quotient : " + quotient);
+            int first = 7;
+            int second = 5;
+            decimal quotient = (decimal)first / (decimal)second;
+            Console.WriteLine(quotient);
         }
     }
 }
