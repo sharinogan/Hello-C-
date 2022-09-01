0c420e544a1375211adcca3ae6e3a490afdff148
Voragun Supawong <voragun2543@gmail.com>
2022-09-01 16:23:35 +0700

    🎁 [if-else statement] Challenge: Improve renewal rate of subscriptions

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,30 +6,30 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Random dice = new Random();
+            Random random = new Random();
+            int daysUntilExpiration = random.Next(12);
+            int discountPercentage = 0;
 
-            int roll1 = dice.Next(1, 7);
-            int roll2 = dice.Next(1, 7);
-            int roll3 = dice.Next(1, 7);
-
-            int total = roll1 + roll2 + roll3;
-
-            Console.WriteLine($"Dice roll : {roll1} + {roll2} + {roll3} = {total}");
-
-            if ((roll1 == roll2) || (roll2 == roll3) || (roll3 == roll1))
+            Console.WriteLine($"{daysUntilExpiration}");
+            if (daysUntilExpiration <= 10)
             {
-                Console.WriteLine("You rolled doubles! +2 bonus to total!");
-                total += 2;
+                Console.WriteLine("Your subscription will expire soon. Renew now!");
             }
-
-            if (total >= 15)
+            else if (daysUntilExpiration <= 5)
             {
-                Console.WriteLine("You win!");
+                Console.WriteLine($"Your subscript expires in {daysUntilExpiration} days");
+                discountPercentage = 10;
+                Console.WriteLine($"Renew now and save {discountPercentage}%");
             }
-
-            if (total < 15)
+            else if (daysUntilExpiration == 1)
             {
-                Console.WriteLine("Sorry, you lose.");
+                Console.WriteLine("Your subscription expores within a day!");
+                discountPercentage = 20;
+                Console.WriteLine($"Renew now and save {discountPercentage}%");
+            }
+            else
+            {
+                Console.WriteLine("Your subscription has expired");
             }
         }
     }
