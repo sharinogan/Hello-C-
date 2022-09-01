80a16e16b29679353d4b395db0f12d71ebd4ccc4
Voragun Supawong <voragun2543@gmail.com>
2022-09-01 15:05:11 +0700

    [variable syntax] Perform Basic Math Operations

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,8 +10,21 @@ namespace HelloWorld
             Console.WriteLine($"Hello, {user}");
 
             string projectName = "First experience";
-            Console.Write($"This is {projectName} Program");
+            Console.Write($"This is {projectName} Program\n"); // string interpolation
 
+            int first = 10;
+            int second = 20;
+            Console.WriteLine(first + second);
+
+            int sum = 7 + 5;
+            int difference = 7 - 5;
+            int product = 7 * 5;
+            int quotient = 7 / 5;
+
+            Console.WriteLine("Sum : " + sum);
+            Console.WriteLine("Difference : " + difference);
+            Console.WriteLine("Product : " + product);
+            Console.WriteLine("Quotient : " + quotient);
         }
     }
 }
